4d0a6b7bcb3a1a2079c8c07f19621419b6b1303a
Rahim Klabér <rahimklaber2@gmail.com>
2024-06-10 15:07:41 +0200

    Fix ContractInstance xdr decode error.

--- src/commonMain/kotlin/me.rahimklaber.stellar/base/xdr/soroban/SCContractInstance.kt
+++ src/commonMain/kotlin/me.rahimklaber.stellar/base/xdr/soroban/SCContractInstance.kt
@@ -1,23 +1,21 @@
 package me.rahimklaber.stellar.base.xdr.soroban
 
-import me.rahimklaber.stellar.base.xdr.XdrElement
-import me.rahimklaber.stellar.base.xdr.XdrElementDecoder
-import me.rahimklaber.stellar.base.xdr.XdrStream
+import me.rahimklaber.stellar.base.xdr.*
 
 data class SCContractInstance(
     val executable: ContractExecutable,
-    val storage: SCMap,
+    val storage: SCMap?,
 ): XdrElement {
     override fun encode(stream: XdrStream) {
         executable.encode(stream)
-        storage.encode(stream)
+        storage.encodeNullable(stream)
     }
 
 
     companion object: XdrElementDecoder<SCContractInstance> {
         override fun decode(stream: XdrStream): SCContractInstance {
             val executable = ContractExecutable.decode(stream)
-            val storage = SCMap.decode(stream)
+            val storage = SCMap.decodeNullable(stream)
 
             return SCContractInstance(executable, storage)
         }
