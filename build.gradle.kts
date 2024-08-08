80fa144ce3c1e57ff80ee375b93babbe4bc7d945
Rahim Klabér <rahimklaber2@gmail.com>
2024-08-08 17:19:56 +0200

    disable native sourceset for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,12 +116,12 @@ kotlin {
     }
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+//    val nativeTarget = when {
+//        hostOs == "Mac OS X" -> macosX64("native")
+//        hostOs == "Linux" -> linuxX64("native")
+//        isMingwX64 -> mingwX64("native")
+//        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+//    }
 
 
     sourceSets {
@@ -164,11 +164,11 @@ kotlin {
             }
         }
         val jsTest by getting
-        val nativeMain by getting{
-            dependencies {
-                implementation("io.ktor:ktor-client-winhttp:$ktor_version")
-        }
-        }
-        val nativeTest by getting
+//        val nativeMain by getting{
+//            dependencies {
+//                implementation("io.ktor:ktor-client-winhttp:$ktor_version")
+//        }
+//        }
+//        val nativeTest by getting
     }
 }
